91c7e928a38fa18a19489c4d6689c53ab0c76339
felipe rodriguez <felipe.rodriguezri@gmail.com>
2024-05-29 22:20:26 -0400

    se crea proyecto desafio evaluado m6-dia12 de vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,99 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container text-center justify-content-center py-3">
+    <main class="main">
+      <h1>Agenda tu cita medica</h1>
+
+      <form class="border rounded pt-2" action="" @submit.prevent="agregarCita">
+        <div class="d-md-flex justify-content-around py-2">
+          <!-- Paciente -->
+          <div>
+            <label class="form-label" for="" :class="!cita.paciente ? 'text-danger' : ''">Paciente</label>
+            <input type="text" class="form-control" v-model="cita.paciente">
+          </div>
+          <!-- Fecha -->
+          <div>
+            <label class="form-label" for="" :class="!cita.fecha ? 'text-danger' : ''">Fecha</label>
+            <input type="date" class="form-control" v-model="cita.fecha">
+          </div>
+          <!-- Hora -->
+          <div>
+            <label class="form-label" for="" :class="!cita.hora ? 'text-danger' : ''">Hora </label>
+            <input type="time" class="form-control" v-model="cita.hora">
+          </div>
+          <!-- Gravedad -->
+          <div>
+            <label class="form-label" for="" :class="!cita.gravedad ? 'text-danger' : ''">Gravedad</label>
+            <select name="" id="" class="form-select" v-model="cita.gravedad">
+              <option :value="gravedad" v-for="(gravedad, index) in gravedades" :key="index">{{ gravedad }}</option>
+            </select>
+          </div>
+          <!-- Motivo -->
+          <div>
+            <label class="form-label" for="" :class="!cita.motivo ? 'text-danger' : ''">Motivo</label>
+            <input type="text" class="form-control" v-model="cita.motivo">
+          </div>
+        </div>
+
+        <!-- Div Boton -->
+        <div class="py-3">
+          <button type="submit" class="btn btn-light" :disabled="!cita.paciente || !cita.fecha || !cita.hora || !cita.gravedad
+            || !cita.motivo">Agregar</button>
+        </div>
+
+      </form>
+      <div class="p-3">
+        <p class="text-danger" v-if="citas.length < 1">Aun no hay consultas registradas</p>
+        <div v-else class="row g-2">
+          <div class="col-3" v-for="(cita, index) in citas" :key="index">
+            <CardComp :cita="cita" @eliminarCita="eliminarCitaHandler(index)" />
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardComp from './components/CardComp.vue';
+
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    CardComp,
+  },
+
+  data() {
+    return {
+      citas: [],
+      cita: {
+        paciente: "",
+        fecha: "",
+        Hora: "",
+        gravedad: "",
+        motivo: "",
+      },
+      gravedades: ["Baja", "Media", "Alta"],
+    }
+  },
+
+  methods: {
+    agregarCita() {
+      this.citas.push(this.cita);
+      this.cita = {};
+    },
+    eliminarCitaHandler(index) {
+      if (confirm("Seguro que deseas eliminar esta cita?")) {
+        this.citas.splice(index, 1)
+      }
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CardComp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="CardComp">
+        <div id="cardPaciente">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body" :class="cita.gravedad == 'Alta' ? 'text-bg-danger' : cita.gravedad == 'Media' ? 'text-bg-warning' : cita.gravedad == 'Baja' ? 'text-bg-success' : ''">
+                    <h6 class=" card-title">Paciente</h6>
+                    <p class="card-text"> {{ cita.paciente }}</p><hr>
+                    <h6 class="card-title">Fecha</h6>
+                    <p class="card-text"> {{ cita.fecha }}</p><hr>
+                    <h6 class="card-title">Hora</h6>
+                    <p class="card-text"> {{ cita.hora }}</p><hr>
+                    <h6 class="card-title">Motivo</h6>
+                    <p class="card-text"> {{ cita.motivo }}</p><hr>
+                    <button href="#" class="btn btn-primary" @click="$emit('eliminarCita')">Eliminar</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "CardComp",
+    props: {
+        cita: Object,
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card {
+border-radius: 1px solid black;
+}
+</style>
